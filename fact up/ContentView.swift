b2541796a5df5ccf45b1f0c0b0c7288ea0eca0e4
michaelkot1 <kotmichael7@gmail.com>
2025-08-05 11:31:16 -0700

    Initial Commit

---
+++ fact up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fact up
+//
+//  Created by Michael Kot on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fact up/fact_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  fact_upApp.swift
+//  fact up
+//
+//  Created by Michael Kot on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct fact_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
